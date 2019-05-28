cbd54c009f7aef8d214f200e35f3d59a44beae7a
cryptoempire <ecehood@gmail.com>
2019-05-28 10:40:52 -0500

    added 4 images

--- portfolio_website/src/components/GalleryGrid/GalleryGrid.js
+++ portfolio_website/src/components/GalleryGrid/GalleryGrid.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
+import etherisc from '../../images/etherisc.png';
+import emerge from '../../images/emerge.png';
+import hivewire from '../../images/hivewire.png';
+import dccper from '../../images/dccper.png';
+
 import './GalleryGrid.css';
 
 const GalleryGrid = () => {
   return (
 <div>
   <div className='gallery'>
-    <div className='item-a'></div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
+    <div className='item-a'><img src={etherisc} /></div>
+    <div className='item-b'><img src={emerge} /></div>
+    <div className='item-c'><img src={hivewire} /></div>
+
+    <div className='item-d'><img src={dccper} /></div>
     <div>hello</div>
     <div>hello</div>
     <div>hello</div>
